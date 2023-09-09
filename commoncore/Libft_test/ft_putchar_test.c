461c4db8549f602e256bf10891c45f5629104a3a
Tian Kian Tan <titan@c1r7s2.42singapore.sg>
2023-09-09 22:51:52 +0800

    wip

--- commoncore/Libft_test/ft_putchar_test.c
+++ commoncore/Libft_test/ft_putchar_test.c
@@ -1,7 +1,47 @@
 #include "libft_test.h"
 
+typedef struct s_test
+{
+	char *desc;
+	char c;
+	char *expected;
+} t_test;
+
 void    ft_putchar_test(void)
 {
-    ft_putchar('a');
-    ft_putchar('\n');
+    	t_test tests[] = {
+	    {.desc = "ft_putchar('c')",
+	     .c = 'c',
+	     .expected = "c"},
+	    {.desc = "ft_putchar('x')",
+	     .c = 'x',
+	     .expected = "x"},
+	    {.desc = "ft_putchar(' ')",
+	     .c = ' ',
+	     .expected = " "},
+	    {.desc = "ft_putchar('\\n')",
+	     .c = '\n',
+	     .expected = "\n"},
+	    {.desc = "ft_putchar('\\t')",
+	     .c = '\t',
+	     .expected = "\t"},
+	    {.desc = "ft_putchar('\\0')",
+	     .c = '\0',
+	     .expected = "\0"},
+	    {.desc = "ft_putchar('\\xFF')",
+	     .c = '\xFF',
+	     .expected = "\xFF"},
+	    // Add more test cases here
+	};
+
+   	int count = sizeof(tests) / sizeof(tests[0]);
+    int i = 0;
+
+    while (i < count)
+    {
+        printf("%s , input: ",tests[i].desc);
+        ft_putchar(tests[i].c);
+        printf("expected: %s\n",tests[i].expected);
+        i++;
+    }
 }
--- commoncore/Libft_test/libft_test.h
+++ commoncore/Libft_test/libft_test.h
@@ -1,2 +1,4 @@
+#include <stdio.h>
+
 void ft_putchar(char c);
 void ft_putchar_test(void);
