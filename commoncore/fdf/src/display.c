19436a24c0269eb8a4ea562a600087b9a00f4483
Tian Kian Tan <titan@c2r4s3.42singapore.sg>
2024-01-22 21:12:32 +0800

    seg fault

--- commoncore/fdf/src/display.c
+++ commoncore/fdf/src/display.c
@@ -6,29 +6,44 @@
 /*   By: titan <titan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 07:02:37 by titan             #+#    #+#             */
-/*   Updated: 2024/01/20 15:12:16 by titan            ###   ########.fr       */
+/*   Updated: 2024/01/22 20:45:42 by titan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-t_display	*create_display()
+static t_display *init_display()
 {
 	t_display	*disp;
 	t_img		*disp_img;
+	void		*mlx;
 
 	disp = (t_display *)malloc(sizeof(t_display));
 	if (!disp)
-		exit_error("create_display malloc error");
+		exit_error("create_display disp malloc error");
 	disp_img = (t_img *)malloc(sizeof(t_img));
 	if (!disp_img)
-		exit_error("create display malloc error");
+		exit_error("create display disp_img malloc error");
+	mlx = (void *)malloc(sizeof(void *));
+	if (!mlx)
+		exit_error("create display mlx malloc error");
 	disp -> img = disp_img;
+	disp -> mlx = mlx;
+	return (disp);
+}
+
+t_display	*create_display()
+{
+	t_display	*disp;
+	t_img 		*img;
+
+	disp = init_display();
+	img = disp -> img;
 	disp -> mlx = mlx_init();
 	disp -> mlx_win = mlx_new_window(disp -> mlx, RESO_X, RESO_Y, "FDF");
-	disp_img -> img = mlx_new_image(disp -> mlx, RESO_X, RESO_Y);
-	disp_img = disp -> img;
-	disp_img -> addr = mlx_get_data_addr(disp_img -> img, &disp_img -> b_in_pix,&disp_img -> l_len, &disp_img -> endian);
+	disp -> img -> img = mlx_new_image(disp -> mlx, RESO_X, RESO_Y);
+	disp -> img = disp -> img;
+	img -> addr = mlx_get_data_addr(img -> img, &img -> b_in_pix,&img -> l_len, &img -> endian);
 	return (disp);
 }
 
--- commoncore/fdf/src/hooks.c
+++ commoncore/fdf/src/hooks.c
@@ -6,7 +6,7 @@
 /*   By: titan <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 15:22:11 by titan             #+#    #+#             */
-/*   Updated: 2024/01/20 18:13:18 by titan            ###   ########.fr       */
+/*   Updated: 2024/01/22 21:11:05 by titan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,21 @@ int	close_prog(void *param)
 	exit(0);
 }
 
+int	key_handle(int keycode, void *param)
+{
+	if (keycode == XK_Escape)
+		close_prog(param);
+	return (0);
+}
+
 void	hook_all(t_display *d, t_map *m)
 {
-	t_param p;
+	t_param *p;
+	
+	p = (t_param *)malloc(sizeof(t_param));
+	p -> d = d;
+	p -> m = m;
+	mlx_hook(d -> mlx_win, 17, XK_Escape,close_prog, p);
+	mlx_key_hook(d -> mlx_win, &key_handle,p);
 
-	p.d = d;
-	p.m = m;
-	mlx_hook(d -> mlx_win, 2, XK_Escape,close_prog, &p);
 }
